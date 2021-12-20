9fa9ca8bbde67efc75a0e76d684641d618ce9572
Andrey <andrey.kalyakin@gmail.com>
2021-12-20 10:30:34 +0300

    Исправлена классификация ошибки парсера - только XML

--- src/parser.js
+++ src/parser.js
@@ -1,26 +1,23 @@
 const parse = (data) => {
-  try {
-    const parserDOM = new DOMParser();
-    const rss = parserDOM.parseFromString(data, 'application/xml');
-    const parsererror = rss.querySelector('parsererror');
-    if (parsererror !== null) {
-      throw new Error(parsererror.textContent);
-    }
-    const channel = rss.querySelector('channel');
-    const items = Array.from(channel.querySelectorAll('item'));
-    return {
-      title: channel.querySelector('title').textContent,
-      description: channel.querySelector('description').textContent,
-      items: items.map((item) => ({
-        title: item.querySelector('title').textContent,
-        description: item.querySelector('description').textContent,
-        link: item.querySelector('link').textContent,
-      })),
-    };
-  } catch (e) {
+  const parserDOM = new DOMParser();
+  const rss = parserDOM.parseFromString(data, 'application/xml');
+  const parsererror = rss.querySelector('parsererror');
+  if (parsererror !== null) {
+    const e = new Error(parsererror.textContent);
     e.isParseError = true;
     throw e;
   }
+  const channel = rss.querySelector('channel');
+  const items = Array.from(channel.querySelectorAll('item'));
+  return {
+    title: channel.querySelector('title').textContent,
+    description: channel.querySelector('description').textContent,
+    items: items.map((item) => ({
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    })),
+  };
 };
 
 export default parse;
